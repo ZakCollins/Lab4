9d4714ed2c9070abf37b8919ecef236b55590b7e
Zak Collins <foazak@yahoo.com>
2014-09-17 16:17:59 -0600

    Change 2

--- Lab 4/src/HelloWorld.java
+++ Lab 4/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		//Editing number 1
+		//Editing number 2
 
 		System.out.println("Hello world!");
 		System.out.println("How are you?");
